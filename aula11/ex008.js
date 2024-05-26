2d594b4073787336d90d45899bce421b5754704a
Wellington <161943926+WellingtonDeLacerda@users.noreply.github.com>
2024-05-26 07:22:20 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 38.4
+console.log(`sua velecoidade é de ${vel}km/h`)
+if(vel > 70){
+    console.log('passou de 70km/h.MULTADO')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`seu país é ${pais}`)
+if(pais != 'Brasil'){
+    console.log('você é ESTRANGEIRO')
+}else{
+    console.log('você é brasileiro')
+}

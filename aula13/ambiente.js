e3517a2fcd33ce5fb1d6acd1934fe398266ab675
Wellington <161943926+WellingtonDeLacerda@users.noreply.github.com>
2024-06-01 12:21:12 -0300

    Add files via upload

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/* forma simples de fazer repetições*/
+
+console.log('tudo ok')
+console.log('tudo ok')
+console.log('tudo ok')
+console.log('tudo ok')
+console.log('tudo ok')
+console.log('tudo ok')
+
+/* formas com while() para fazer repetição*/
+
+let pos = 1
+while (pos <= 6) {
+    console.log('ola tudo jóia')
+    pos++
+}
+
+/* formas com do while() para fazer repetições*/
+
+let pos2 = 1
+do {
+    console.log('eai mano belê')
+    pos2++
+} while (pos2 <= 6);
+
+/* formas com for para fazer repetições */
+
+for (i = 1; i <= 6; i++) {
+    console.log(`${i}° repetição`)
+}

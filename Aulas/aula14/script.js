24fb971c5efdb6934ab3ec5240ee31d1831617b8
Wellington <161943926+WellingtonDeLacerda@users.noreply.github.com>
2025-03-30 08:21:04 -0300

    Add files via upload

---
+++ Aulas/aula14/script.js
@@ -0,0 +1,9 @@
+let r = document.querySelector("#res")
+for(let c = 0;c <= 10;c++){
+    console.log(c)
+    r.innerHTML += `texto lorem <br>`
+}
+console.log("______")
+for(let s = 1;s <= 5;s++){
+    console.log(s)
+}
